d37e7663989fc3df31e686abc62106605db683a5
soniapop93 <popsonia85@gmail.com>
2023-10-28 22:14:17 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        if (args[0].contains("--server")) {
+            //todo: implement it
+        }
+        else if (args[0].contains("--client")) {
+            //todo: implement it
+        }
+    }
+}
